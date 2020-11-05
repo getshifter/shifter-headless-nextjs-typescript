4bb5e0dee7b579d63564a8cdc88355a009a1d7dd
Hidetaka Okamoto <info@wp-kyoto.net>
2020-11-05 19:48:36 +0900

    feat: list wp

---
+++ components/archives/Posts.tsx
@@ -0,0 +1,74 @@
+import React, {FC, ReactNode, useMemo} from 'react'
+import Link from 'next/link'
+import { List, Avatar, Space } from 'antd';
+import { WPPost } from '../../libs/wpapi/interfaces'
+import { formatPostDateToString, getThePostAuthor, getThePostFeatureImage } from '../../libs/wpapi/format';
+
+export const ArchiveThumbnail: FC<{
+    post: WPPost
+}> = ({post}) => {
+    const featuredImage = getThePostFeatureImage(post, 'medium')
+    return featuredImage ? (
+        <img
+          src={featuredImage.source_url}
+          alt={featuredImage.alt_text}
+          width={featuredImage.witdh}
+          height={featuredImage.height}
+        />
+      ): null
+}
+
+export const PostArchiveItem = (post: WPPost): ReactNode => {
+    const author = getThePostAuthor(post)
+    const terms = post._embedded['wp:term']
+    const actions = useMemo(() => {
+        if (!terms) return null
+        return terms.map(termItems => {
+            if (!termItems) return null;
+            return termItems.map(term => (
+              <Space key={term.id}>{term.name}</Space>
+            ))
+          }).flat()
+    }, [terms])  
+
+    return (
+      <List.Item
+        key={post.id}
+        extra={<ArchiveThumbnail post={post} />}
+        actions={actions}
+      >
+        <List.Item.Meta
+          avatar={
+            author ? (
+              <Avatar src={author.source_url} />
+            ): null
+          }
+          title={<Link href="/">
+            <span
+              style={{
+                fontSize: "1.5rem"
+              }}
+              dangerouslySetInnerHTML={{
+                __html: post.title.rendered
+              }}
+            />
+          </Link>}
+          description={formatPostDateToString(post)}
+        />
+        <div dangerouslySetInnerHTML={{
+          __html: post.excerpt.rendered.substr(0, 300)
+        }} />
+      </List.Item>
+    )
+}
+
+export const PostArchives: FC<{
+    posts: WPPost[]
+}> = ({posts}) => (
+    <List
+    itemLayout="vertical"
+    size="large"
+    dataSource={posts}
+    renderItem={PostArchiveItem}
+  />
+)
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,21 @@
 import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import { FC } from 'react'
-import Link from 'next/link'
-import { List, Avatar, Space } from 'antd';
+import useSWR from 'swr'
 import { fetch } from '../libs/polyfil/fetch'
 import { WPPost } from '../libs/wpapi/interfaces'
 import { WPAPIURLFactory } from '../libs/wpapi/UrlBuilder'
-import { formatPostDateToString, getThePostAuthor, getThePostFeatureImage } from '../libs/wpapi/format';
+import { PostArchives } from '../components/archives/Posts';
 
 const urlBuilder = WPAPIURLFactory.init(
-  'https://f22ea9dbddf63a6e3b8921370b53a7cd21f0263c.hl-b.getshifter.co/wp-json'
+  process.env.WORDPRESS_URL,
 ).postType('posts').startAt(1).withEmbed()
 
 
 export const Home:FC<{
   posts: WPPost[]
-}> = ({posts}) => {
+}> = ({posts: initialProps}) => {
+  const {data: posts} = useSWR<Array<WPPost>>(urlBuilder.getURL(), fetch, {initialData: initialProps})
   return (
     <div>
       <Head>
@@ -23,57 +23,7 @@ export const Home:FC<{
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>Recent posts</h1>
-      <List
-        itemLayout="vertical"
-        size="large"
-        dataSource={posts}
-        renderItem={post => {
-          const featuredImage = getThePostFeatureImage(post, 'medium')
-          const author = getThePostAuthor(post)
-          const terms = post._embedded['wp:term']          
-          return (
-            <List.Item
-              key={post.id}
-              extra={featuredImage ? (
-                <img
-                  src={featuredImage.source_url}
-                  alt={featuredImage.alt_text}
-                  width={featuredImage.witdh}
-                  height={featuredImage.height}
-                />
-              ): null}
-              actions={terms ? terms.map(termItems => {
-                if (!termItems) return null;
-                return termItems.map(term => (
-                  <Space key={term.id}>{term.name}</Space>
-                ))
-              }).flat(): null}
-            >
-              <List.Item.Meta
-                avatar={
-                  author ? (
-                    <Avatar src={author.source_url} />
-                  ): null
-                }
-                title={<Link href="/">
-                  <span
-                    style={{
-                      fontSize: "1.5rem"
-                    }}
-                    dangerouslySetInnerHTML={{
-                      __html: post.title.rendered
-                    }}
-                  />
-                </Link>}
-                description={formatPostDateToString(post)}
-              />
-              <div dangerouslySetInnerHTML={{
-                __html: post.excerpt.rendered.substr(0, 300)
-              }} />
-            </List.Item>
-          )
-        }}
-      />
+      <PostArchives posts={posts} />
     </div>
   )
 }
